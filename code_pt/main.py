c0835758c71db9e42fff47d75c316cb91de4d338
Eb <160401318+e43b@users.noreply.github.com>
2024-02-27 10:26:11 -0300

    Update main.py

--- code_pt/main.py
+++ code_pt/main.py
@@ -2,9 +2,10 @@ import os
 
 print("\nPara baixar um único post com imagens digite 1")
 print("Para baixar um único post de texto digite 2")
-print("Para baixar todos os posts com imagens de um perfil digite 3")
-print("Para baixar todos os posts de texto perfil digite 4")
-print("Para baixar todas as DMs de um perfil digite 5")
+print("Para baixar um post completo digite 3")
+print("Para baixar todos os posts com imagens de um perfil digite 4")
+print("Para baixar todos os posts de texto perfil digite 5")
+print("Para baixar todas as DMs de um perfil digite 6")
 
 opcao = input("\nDigite sua opção: ")
 
@@ -15,12 +16,15 @@ elif opcao == "2":
     # Executar script para baixar posts de texto
     os.system("python scripts/postmessages.py")
 elif opcao == "3":
+    # Executar script para baixar um post completo
+    os.system("python scripts/postfull.py")
+elif opcao == "4":
     # Executar script para baixar todos os posts com imagens de um perfil
     os.system("python scripts/profileimg.py")
-elif opcao == "4":
+elif opcao == "5":
     # Executar script para baixar todos os posts de texto de um perfil
     os.system("python scripts/profilemessages.py")
-elif opcao == "5":
+elif opcao == "6":
     # Executar script para baixar todas as DMs um perfil
     os.system("python scripts/profiledms.py")
 else:
