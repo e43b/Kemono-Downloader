0a71fbba3343f976ffc5c879092fe4048309fe13
Eb <160401318+e43b@users.noreply.github.com>
2024-02-27 10:26:35 -0300

    Update main.py

--- code_en/main.py
+++ code_en/main.py
@@ -2,25 +2,29 @@ import os
 
 print("\nTo download a single post with images, type 1")
 print("To download a single text post, type 2")
-print("To download all posts with images from a profile, type 3")
-print("To download all text posts from a profile, type 4")
-print("To download all DMs from a profile, type 5")
+print("To download a complete post, type 3")
+print("To download all posts with images from a profile, type 4")
+print("To download all text posts from a profile, type 5")
+print("To download all DMs from a profile, type 6")
 
-opcao = input("\nEnter your option: ")
+option = input("\nEnter your option: ")
 
-if opcao == "1":
-    # Execute script to download posts with images
+if option == "1":
+    # Execute the script to download posts with images
     os.system("python scripts/postimg.py")
-elif opcao == "2":
+elif option == "2":
     # Execute script to download text posts
     os.system("python scripts/postmessages.py")
-elif opcao == "3":
+elif option == "3":
+    # Execute script to download a complete post
+    os.system("python scripts/postfull.py")
+elif option == "4":
     # Execute script to download all posts with images from a profile
     os.system("python scripts/profileimg.py")
-elif opcao == "4":
+elif option == "5":
     # Execute script to download all text posts from a profile
     os.system("python scripts/profilemessages.py")
-elif opcao == "5":
+elif option == "6":
     # Execute script to download all DMs from a profile
     os.system("python scripts/profiledms.py")
 else:
